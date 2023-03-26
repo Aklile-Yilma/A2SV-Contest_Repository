bb9d89c720d84d76b69e4dcb66eec39772f168b1
Aklile Yilma <aklileyilma@gmail.com>
2023-03-26 13:07:20 +0300

    quick sort implemented

--- random/sorting/quick_sort.py
+++ random/sorting/quick_sort.py
@@ -1,35 +1,30 @@
-
-
 def quick_sort(arr, start, end):
     #base case: empty or len(arr) == 1
-    if end - start + 1 <= 1:
+    if end - start + 1  <= 1:
         return arr
     
     pivot = arr[start]
     pivot_idx = start
-    left = start + 1
+    left = start 
     
     # partition elements smaller than pivot on the left side
     # partition elements greater than pivot on the right side
-    
-    for right in range(start+1, end):
-        print(arr,left, right)
+    for right in range(start, end+1):
         if arr[right] <= pivot:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
-                        
+    
     # move pivot in-between left and right sides
-    print(arr)
-    if left > end-1:
-        left = end - 1
-            
+
+    left -= 1        
     arr[left], arr[pivot_idx] = arr[pivot_idx], arr[left]
     
-    print("after pivot swap", arr)
     quick_sort(arr, start, left-1)
-    quick_sort(arr, left + 1, end)
-    
+    quick_sort(arr, left+1, end)
     return arr
 
-res = quick_sort([6, 2, 4, 1, 3], 0, 5)
+arr = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+arr2 = [29,10,14,37,14]
+ 
+res = quick_sort(arr, 0, len(arr)-1)
 print(res)
